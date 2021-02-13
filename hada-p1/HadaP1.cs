7d2318c85eca39741dc113afc74760f3c2210d28
Nicolás Van Olmen Giménez <nvog1@alu.ua.com>
2021-02-13 21:55:22 +0100

    Implemented main

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -10,6 +10,31 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string unidades;
+            string str_cantidad;
+            string seguir;
+            double double_cantidad;
+
+            do
+            {
+                Console.WriteLine("¿Qué unidades usas?(m/s)");
+                unidades = Console.ReadLine();
+
+                Console.WriteLine("Introduce la cantidad");
+                str_cantidad = Console.ReadLine();
+                double_cantidad = double.Parse(str_cantidad);
+                if(unidades == "m")
+                {
+                    Console.WriteLine(str_cantidad + " minutos son {0} segundos", Minutes2Seconds(double_cantidad));
+                }
+                else
+                {
+                    Console.WriteLine(str_cantidad + " segundos son {0} minutos", Seconds2Minutes(double_cantidad));
+                }
+
+                Console.WriteLine("¿Quieres seguir?(s/n)");
+                seguir = Console.ReadLine();
+            } while (seguir != "n");
         }
 
         public static double Seconds2Minutes(double s)
