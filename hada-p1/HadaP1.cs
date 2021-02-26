e700539c15aab699bf60bfce609568702b3b98d4
Nicolás Van Olmen Giménez <nvog1@alu.ua.com>
2021-02-26 22:36:27 +0100

    Documented HadaP1.cs

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Clase que contiene métodos para pasar de minutos a segundos y viceversa
+    /// </summary>
     class HadaP1
     {
+        /// <summary>
+        /// Interfaz usuario-programa para concretar unidades y cantidad
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string unidades;
@@ -37,6 +44,11 @@ namespace hada_p1
             } while (seguir != "n");
         }
 
+        /// <summary>
+        /// Método que transforma de segundos a minutos
+        /// </summary>
+        /// <param name="s">Cantidad de segundos a convertir</param>
+        /// <returns>Cantidad de segundos convertida a minutos</returns>
         public static double Seconds2Minutes(double s)
         {
             if(s == 0)
@@ -50,6 +62,11 @@ namespace hada_p1
                
         }
 
+        /// <summary>
+        /// Método que transforma de minutos a segundos
+        /// </summary>
+        /// <param name="m">Cantidad de minutos a convertir</param>
+        /// <returns>Cantidad de minutos convertida a segundos</returns>
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
